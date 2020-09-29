7e5e9453372c7cf131a19cd0fdc0efc5c87fcc07
Корниенко Алексей Александрович <Kornienko-A@gis.lan>
2020-09-29 21:54:19 +0300

    feat: finished task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+const sorter = (a, b) => {
+  if (a > b) {
+    return 1;
+  } else if (a < b) {
+    return -1;
+  }
+  return 0;
+};
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  return array.splice(0).sort(sorter)[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  return array.splice(0).sort(sorter).splice(-1)[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  return array.reduce((acc, item) => acc + item, 0) / array.length;
 }
